b318b1ae3f2351531bf83ca864235158f456dc72
Sveta2507 <dnsdns.dn@goglemail.com>
2020-06-25 15:14:10 +0300

    header & footer are done

---
+++ js/classwork.js
@@ -0,0 +1,19 @@
+// переменные let and const
+let name; //объявление переменной
+console.log(name);//undefined
+
+name = "Aleksa"; //инициализация переменной
+console.log(name); // Aleksa
+
+name = "Aleksandra"; // переприсвоение значения
+console.log(name); // Alrsandra
+
+// const lastName; - нельзя объявить без зачения
+const lastName = "Hrevtsova";// объявляем и инициализируем одновременно
+console.log(lastName);
+
+//lastName = "ihefoehdoew"; - нельзя переназначить
+
+// типы данных
+
+//примитивы
