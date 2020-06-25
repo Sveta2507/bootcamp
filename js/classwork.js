2eec32282f69378bbe52ac729e7951c5dd8f3352
Sveta2507 <dnsdns.dn@goglemail.com>
2020-06-25 20:50:55 +0300

    js

--- js/classwork.js
+++ js/classwork.js
@@ -2,10 +2,10 @@
 let name; //объявление переменной
 console.log(name);//undefined
 
-name = "Aleksa"; //инициализация переменной
+name = 'Aleksa'; //инициализация переменной
 console.log(name); // Aleksa
 
-name = "Aleksandra"; // переприсвоение значения
+name = 'Aleksandra'; // переприсвоение значения
 console.log(name); // Alrsandra
 
 // const lastName; - нельзя объявить без зачения
@@ -17,3 +17,41 @@ console.log(lastName);
 // типы данных
 
 //примитивы
+// 1 - string ('',"",``)
+let myName = 'Aleksandra';
+console.log(name); //aleksandra
+console.log(typeof myName); //string
+
+let count = '30';
+console.log(count);
+console.log(typeof age); //number
+
+// 2 - number
+let age = 30
+console.log(age);
+console.log(typeof age); //number
+
+// 3 - boolean (true,false)
+let isOnline = true;
+console.log(isOnline); //true
+console.log(typeof true); //boolean
+
+isOnline = false;
+console.log(isOnline); //false
+console.log(typeof isOnline);
+
+// 4 - undefined
+let products;
+console.log(products);
+console.log(typeof products); //undefined
+
+// 5 - null
+let cart = null;
+console.log(cart); //null
+console.log(typeof cart); // object - специальная ошибка в js
+
+// сложные
+// object
+// array
+// function
+// symbol
