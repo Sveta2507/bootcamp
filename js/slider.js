8ac251c67872a2cd5454f31421804f2615eeae3a
Sveta2507 <dnsdns.dn@goglemail.com>
2020-07-01 15:12:32 +0300

    js

--- js/slider.js
+++ js/slider.js
@@ -1,23 +1,35 @@
 //1 секция
-const logo = document.querySelector(".s3 ant-carousel");
-console.dir(logo);
+const s3 = document.getElementsByClassName(".s3 ant-carousel");
+console.log(ant - carousel);
+console.dir(ant - carousel);
+console.log(ant - carousel[0].textContent);
+
 //2 див
-const logo = document.querySelector(".amt-carousel-hider");
-console.dir(logo);
+const hider = document.getElementsByClassName(".amt-carousel-hider");
+console.log(amt - carousel - hider);
+console.dir(amt - carousel - hider);
+console.log(amt - carousel - hider[0].textContent);
+
 //3 див с левой стрелкой
-const logo = document.querySelector(".ant-carousel-arrow-left");
-console.dir(logo);
+const arrow_left = document.querySelector(".ant-carousel-arrow-left");
+console.log(ant - carousel - arrow - left);
+console.dir(ant - carousel - arrow - left);
+console.log(ant - carousel - arrow - left[0].textContent);
+
 //4 список ю эль
 const logo = document.querySelector(".amt-carousel-list");
-console.dir(logo);
+console.log(amt - carousel - list);
+console.dir(amt - carousel - list);
+console.log(amt - carousel - list[0].textContent);
+
 //5 див с правой стрелкой
-const logo = document.getElementsById("ant-carousel-arrow-right");
-console.log(logo);
-console.dir(logo);
-console.log(logo[0].textContent);
+const arrow_right = document.getElementsById("ant-carousel-arrow-right");
+console.log(ant - carousel - arrow - right);
+console.dir(ant - carousel - arrow - right);
+console.log(ant - carousel - arrow - right[0].textContent);
 
 //6 див с индикаторными точками
-const logo = document.getElementsById("ant-carousel-dots");
-console.log(logo);
-console.dir(logo);
-console.log(logo[0].textContent);
+const dots = document.getElementsById("ant-carousel-dots");
+console.log(ant - carousel - dots);
+console.dir(ant - carousel - dotso);
+console.log(ant - carousel - dots[0].textContent);
