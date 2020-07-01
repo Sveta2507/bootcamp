bfff10d35e1787885de78f636279585ae725b57b
Sveta2507 <dnsdns.dn@goglemail.com>
2020-07-01 20:07:22 +0300

    slider in progress

--- js/slider.js
+++ js/slider.js
@@ -1,35 +1,90 @@
+"use strict";
+
+const Ant = function (carouselId) {
+  let carouselId = document.getElementById(carouselId);
+  if (carouselId) {
+    this.carouselRoot = carouselId;
+  } else {
+    this.carouselRoot = document.querySelector(".ant-carousel");
+  }
+  //все объекты нашего слайдера
+  this.list = this.carouselRoot.querySelector(".amt-carousel-list");
+  this.items = this.carouselRoot.querySelectorAll(".amt-carousel-element");
+  this.firstItem = this.carouselRoot.querySelector("amt-carousel-element");
+  this.left_arrow = this.carouseRoot.querySelector(".ant-carousel-arrow-left");
+  this.right_arrow = this.carouseRoot.querySelector(
+    ".ant-carousel-arrow-right"
+  );
+  this.dots = this.carouselRoot.querySelector(".ant-carousel-dots");
+};
+
+Ant.defaults = {
+  visibleItem: 1,
+  loop: true,
+  auto: true,
+  interval: 2000,
+  speed: 1500,
+  touch: true,
+  arrows: true,
+  dots: true,
+};
+
+Ant.prototype.elementPrev = function (num) {
+  num = num || 1;
+  if (this.options.dots) this.dotOn(this.currentElements);
+  this.currentElement -= num;
+  if (this.currentElement < 0) this.currentElement = this.visibleItem - 1;
+  if (this.options.dots) this.dorOff(this.currentElement);
+
+  if (this.options.loop) {
+    this.currentOffset += this.elemWidth * num;
+    this.list.style.marginLeft = this.currentOffset + "px";
+    if (this.currentElement == 0) {
+      this.left_arrow.style.display = "none";
+      this.touchPrev = false;
+    }
+    this.right_arrow.style.display = "block";
+    this.touchNext = true;
+  } else {
+    let elem,
+      buf,
+      this$ = this;
+
+    for (let i = 0; i < num; i++) {
+      //   записываем ссылку на последний элемент списка в переменную элем
+      elem = this.list.lastElementChild;
+      // сщздаём полный - глубокий клон послденего элемента списка
+      buf = elem.cloneNode(true);
+      // встраиваем последний склонированый элемент списка в самое начало списка
+      this.list.insertBefore(buf, this.list.firstElementChild);
+      // удаляем послдений элемент списка, из которого делали клон
+      elem.remove;
+    }
+    this.list.style.cssText =
+      "Transition:margin" + this.options.speed + "ms ease;";
+    this.list.style.marginLeft = "0px";
+    setTimeout(() => {
+      this$.list.style.cssText = "Transition:none";
+    }, this.options.speed);
+  }
+};
+
+Ant.prototype.elementNext = function (num) {};
+
 //1 секция
-const s3 = document.getElementsByClassName(".s3 ant-carousel");
-console.log(ant - carousel);
-console.dir(ant - carousel);
-console.log(ant - carousel[0].textContent);
+const s3 = document.querySelector(".ant-carousel");
 
 //2 див
-const hider = document.getElementsByClassName(".amt-carousel-hider");
-console.log(amt - carousel - hider);
-console.dir(amt - carousel - hider);
-console.log(amt - carousel - hider[0].textContent);
+const hider = document.querySelector(".amt-carousel-hider");
 
 //3 див с левой стрелкой
 const arrow_left = document.querySelector(".ant-carousel-arrow-left");
-console.log(ant - carousel - arrow - left);
-console.dir(ant - carousel - arrow - left);
-console.log(ant - carousel - arrow - left[0].textContent);
 
 //4 список ю эль
-const logo = document.querySelector(".amt-carousel-list");
-console.log(amt - carousel - list);
-console.dir(amt - carousel - list);
-console.log(amt - carousel - list[0].textContent);
+const list = document.querySelector(".amt-carousel-list");
 
 //5 див с правой стрелкой
-const arrow_right = document.getElementsById("ant-carousel-arrow-right");
-console.log(ant - carousel - arrow - right);
-console.dir(ant - carousel - arrow - right);
-console.log(ant - carousel - arrow - right[0].textContent);
+const arrow_right = document.querySelector(".ant-carousel-arrow-right");
 
 //6 див с индикаторными точками
-const dots = document.getElementsById("ant-carousel-dots");
-console.log(ant - carousel - dots);
-console.dir(ant - carousel - dotso);
-console.log(ant - carousel - dots[0].textContent);
+const dots = document.querySelector(".ant-carousel-dots");
