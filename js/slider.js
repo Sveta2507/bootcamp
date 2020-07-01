2fffc826b2c19053d29f5e1acfeafd4d912c1ed8
Sveta2507 <dnsdns.dn@goglemail.com>
2020-07-01 14:55:19 +0300

    js

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,23 @@
 //1 секция
+const logo = document.querySelector(".s3 ant-carousel");
+console.dir(logo);
 //2 див
+const logo = document.querySelector(".amt-carousel-hider");
+console.dir(logo);
 //3 див с левой стрелкой
-//5 список ю эль
-//4 див с правой стрелкой
+const logo = document.querySelector(".ant-carousel-arrow-left");
+console.dir(logo);
+//4 список ю эль
+const logo = document.querySelector(".amt-carousel-list");
+console.dir(logo);
+//5 див с правой стрелкой
+const logo = document.getElementsById("ant-carousel-arrow-right");
+console.log(logo);
+console.dir(logo);
+console.log(logo[0].textContent);
+
 //6 див с индикаторными точками
+const logo = document.getElementsById("ant-carousel-dots");
+console.log(logo);
+console.dir(logo);
+console.log(logo[0].textContent);
